2169cd9ec5e5f9aca287dc26f03c3fb70a03eeb6
Samuel Gil <samuxiii@gmx.com>
2017-05-22 22:27:45 +0200

    Adapt the data and use normalization

--- learning/main.py
+++ learning/main.py
@@ -6,18 +6,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.loadtxt(fname = 'data.txt', delimiter = ',')
-X, y = data[:,:1], data[:,6]
+X, y = data[:,:5], data[:,5]
+
+print X
+print y
 
 m = X.shape[0] #number of samples
 
-X_test = X[1,:]
-y_test = y[1]
+X_test = X[0,:]
+y_test = y[0]
 
-clf = RidgeCV(alphas = [0.1, 1.0, 10.0])
+clf = RidgeCV(alphas = [0.1, 1.0, 10.0], normalize=True)
 clf.fit(X, y)
 
 prediction = clf.predict(X_test);
-print 'X_test is: %f' % X_test
+print 'X_test is: %f'
+print X_test
 print 'Expected is: %f' % y_test
 print 'Prediction is: %f' % prediction
 
@@ -30,8 +34,8 @@ print "Accuracy: %f" % accuracy
 print "Score: %f" % clf.score(X, y)
 
 plt.figure(1)
-plt.plot(X, y, 'ro')
-plt.plot(X, clf.predict(X), 'g-')
+plt.plot(X[:,0], y, 'bo')
+plt.plot(X[:,0], clf.predict(X), 'gx')
 plt.ylabel('Ys')
 plt.xlabel('Xs')
 plt.show()
