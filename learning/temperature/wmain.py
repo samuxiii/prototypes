dfe91553fe298b8e78a98e7b85fb2d19b9c42d0a
Samuel Gil <samuxiii@gmx.com>
2017-05-29 21:14:50 +0200

    X axis shows the dates and 0ºC line is shown

--- learning/temperature/wmain.py
+++ learning/temperature/wmain.py
@@ -4,14 +4,16 @@ from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.dates import date2num
+#from matplotlib.dates import date2num
+import matplotlib.dates as mdates
 from datetime import datetime
 
 def datestr2num(s):
     return date2num(datetime.strptime(s, '%Y-%m-%d'))
 
-data = np.loadtxt(fname = 'weather.txt', delimiter = ',', converters = {0: datestr2num})
-X, y = data[:,1:4], data[:,4]
+data = np.loadtxt(fname = 'weather.txt', delimiter = ',', converters = {0: mdates.strpdate2num('%Y-%m-%d')})
+print data
+X0, X, y = data[:,0], data[:,0:4], data[:,4]
 
 print X
 print y
@@ -24,6 +26,7 @@ clf = RidgeCV(alphas = [0.1, 1.0, 10.0], normalize=True)
 clf.fit(X_train, y_train)
 
 prediction = clf.predict(X_test);
+
 print 'X_test is: %f'
 print X_test
 print 'Expected is: %f' % y_test[0]
@@ -31,12 +34,21 @@ print 'Prediction is: %f' % prediction[0]
 print "Score: %f" % clf.score(X, y)
 print "Alpha: %f" % clf.alpha_
 
+#plotting
 plt.figure(1)
-real, = plt.plot(np.arange(m), y, 'b-', label='real')
-predicted, = plt.plot(np.arange(m), clf.predict(X), 'r-', label='predicted')
+real, = plt.plot(X0, y, 'b-', label='real')
+predicted, = plt.plot(X0, clf.predict(X), 'r-', label='predicted')
+
+#gca = get current axis
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+plt.axhline(y=0, color='g')
+#gcf = get current figure
+plt.gcf().autofmt_xdate()
+
+#legend
 plt.ylabel('Temperature')
 plt.xlabel('Date')
-
 plt.legend([real, predicted], ['Real', 'Predicted'])
 
 plt.show()
