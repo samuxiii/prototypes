f8ac71b393c3077ad581e64d4321e41151362b37
Samuel Gil <samuxiii@gmx.com>
2017-06-04 15:38:41 +0200

    Example of simple neural network

---
+++ learning/numpy/nn-2_to_1-sigmoid.py
@@ -0,0 +1,18 @@
+'''
+Simple network with two input nodes and 
+one output node with a sigmoid activation function.
+'''
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+inputs = np.array([0.7, -0.3])
+weights = np.array([0.1, 0.8])
+bias = -0.1
+
+# y = f(h) = sigmoid(∑(w*x + b))
+output = sigmoid(inputs.dot(weights.T) + bias)
+
+print('Output:')
+print(output)
