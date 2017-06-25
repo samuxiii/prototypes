9009e28402603a7c11036667141ac3da2663af3b
Samuel Gil <samuxiii@gmx.com>
2017-06-25 21:16:55 +0200

    Added new distribution adaptations by range

--- learning/weights/comparative.py
+++ learning/weights/comparative.py
@@ -1,15 +1,18 @@
 import tensorflow as tf
+import numpy as np
 import matplotlib.pyplot as plt
 
 n = 1000
 
 A = tf.random_uniform([n])
+A1 = tf.random_uniform([n], -1/np.sqrt(1000), 1/np.sqrt(1000))
 B = tf.random_normal([n])
 C = tf.truncated_normal([n])
 D = tf.truncated_normal([n], stddev=0.5)
+D1 = tf.truncated_normal([n], stddev=0.1)
 
 with tf.Session() as sess:
-    a, b, c, d = sess.run([A, B, C, D])
+    a, a1, b, c, d, d1 = sess.run([A, A1, B, C, D, D1])
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
 bins_ = 100
@@ -17,20 +20,34 @@ range_ = (-5, 5)
 
 plt.figure("Distribution Comparative")
 
-plt.subplot(221)
+plt.subplot(231)
 plt.hist(a, bins_, range_, label="Random Uniform", color='b')
-plt.legend(fontsize='small')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0., fontsize='x-small')
 
-plt.subplot(222)
+plt.subplot(232)
+plt.hist(a1, bins_, range_, label="Random Uniform 1/sqrt(n)", color='c')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0., fontsize='x-small')
+
+plt.subplot(233)
 plt.hist(b, bins_, range_, label="Random Normal", color='g')
-plt.legend(fontsize='small')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0., fontsize='x-small')
 
-plt.subplot(223)
+plt.subplot(234)
 plt.hist(c, bins_, range_, label="Truncated Normal", color='r')
-plt.legend(fontsize='small')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0., fontsize='x-small')
 
-plt.subplot(224)
+plt.subplot(235)
 plt.hist(d, bins_, range_, label="Truncated Normal stddev=0.5", color='y')
-plt.legend(fontsize='small')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0., fontsize='x-small')
+
+plt.subplot(236)
+plt.hist(d1, bins_, range_, label="Truncated Normal stddev=0.1", color='orange')
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0., fontsize='x-small')
 
 plt.show()
