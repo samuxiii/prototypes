b9b6f30cec0801a2cad87d49e5b852cfa92b5026
Samuel Gil <samuxiii@gmx.com>
2018-01-15 19:53:48 +0200

    Factorial example

---
+++ cpp/fact.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int factorial(int n)
+{
+    if (n == 1) 
+        return 1;
+    return factorial(n-1)*n;
+}
+
+int main()
+{
+    std::cout << factorial(5) << std::endl;
+}
