48d1f4f2f1f18736e91758b642a34965cc87d613
Samuel Gil <samuxiii@gmx.com>
2018-03-11 14:02:40 +0100

    Replay method implemented

--- aigym/cartpole.py
+++ aigym/cartpole.py
@@ -21,6 +21,8 @@ class Agent:
 
         model = Sequential()
         model.add(Dense(2, input_dim=features, activation='relu'))
+
+        #the loss function will be MSE between the action-value Q
         model.compile(loss='mse', optimizer=Adam(lr=learning_rate))
 
         return model
@@ -31,7 +33,28 @@ class Agent:
 
     def replay(self):
         #fit model from memory
-        pass
+        gamma = 0.90
+
+        #take care the memory could be big, so using minibatch
+        size = 16 if len(self.memory) > 16 else len(self.memory)
+        minibatch = random.sample(self.memory, size)
+
+        for state, action, reward, next_state, done in minibatch:
+
+            if not done: #calculate discounted reward
+                action_values = self.model.predict(next_state)[0]
+                #following the formula of action-value expectation
+                reward = reward + gamma * np.amax(action_values)
+
+            target = self.model.predict(state)
+
+            #customize the reward for this certain action using 
+            #the one found in the memory
+            target[0][action] = reward
+
+            #train the model
+            self.model.fit(state, target, epochs=1, verbose=0)
+
 
     def act(self, state):
         #predict the action to do
@@ -52,12 +75,18 @@ if __name__ == "__main__":
             env.render()
             print("state (cart_pos, cart_vel, pole_ang, pol_vel):\n{}".format(state))
 
+            #row vector
+            state = state.reshape(1, -1)
+
             #perform the action
             # 0->left, 1->right
-            action = agent.act(state.reshape(1,-1))
+            action = agent.act(state)
             print("action: {}".format(action))
 
             next_state, reward, done, info = env.step(action)
+
+            #row vector
+            next_state = next_state.reshape(1, -1)
 
             #save the current observation
             agent.remember(state, action, reward, next_state, done)
