054dc07f981bc642709bc83895bb1e5aae89f1b6
Samuel Gil <samuxiii@gmx.com>
2018-03-17 13:08:36 +0100

    Policy_evaluation split into getClosest and certain_policy

--- aigym/frozenlake.py
+++ aigym/frozenlake.py
@@ -16,10 +16,10 @@ def equiprobable_policy():
     actions = 4
     return np.ones([states, actions]) / actions
 
-def policy():
+def certain_policy():
     states = 64
     actions = 4
-    P = np.array([states, actions])
+    P = np.zeros([states, actions])
 
     for s in range(0, states):
        #corners
@@ -47,7 +47,32 @@ def policy():
     return P
 
 #Iterative Policy Evaluation
-def policy_evaluation(num_states):
+def getClosest(state):
+    #return left, down, right, up
+    #corners
+    if state == 0:
+        return 0, 8,  1, 0
+    elif s == 7:
+        return 6,  15, 0, 0
+    elif s == 56:
+        return 0, 0, 57, 48
+    elif s == 63:
+        return 62, 0, 0,  55
+    #sides
+    elif s in [1,2,3,4,5,6]:
+        return state-1, state+8, state+1, 0
+    elif s in [57,58,59,60,61,62]:
+        return state-1, 0, state+1, state-8
+    elif s in [8,16,24,32,40,48]:
+        return 0, state+8, state+1, state-8
+    elif s in [15,23,31,39,47,55]:
+        return state-1, state+8, 0, state-8
+    #other
+    else:
+        return state-1, state+8, state+1, state-8
+
+
+def policy_evaluation(num_states, policy):
     gamma = 1.0
     theta = 1e-4 #max difference between old and new values
     states = range(0, num_states)
@@ -58,38 +83,12 @@ def policy_evaluation(num_states):
 
         for s in states:
             v = V[s]
+            left, down, right, up = getClosest(state)
 
-            #print("state: {}".format(s))
-            #corners
-            if s == 0:
-                #pl, pd, pr, pu = 0, 1/2, 1/2, 0
-                V[s] = 1/2 * (V[8] + V[1])
-            elif s == 7:
-                #pl, pd, pr, pu = 1/2, 1/2, 0, 0
-                V[s]  = 1/2 * (V[6] + V[15])
-            elif s == 56:
-                #pl, pd, pr, pu = 0, 0, 1/2, 1/2
-                V[s] = 1/2 * (V[48] + V[57])
-            elif s == 63:
-                #pl, pd, pr, pu = 1/2, 0, 0, 1/2
-                V[s] = 1/2 * (V[62] + V[55])
-            #sides
-            elif s in [1,2,3,4,5,6]:
-                #pl, pd, pr, pu = 1/3, 1/3, 1/3, 0
-                V[s] = 1/3 * (V[s+8] + V[s+1] + V[s-1])
-            elif s in [57,58,59,60,61,62]:
-                #pl, pd, pr, pu = 1/3, 0, 1/3, 1/3
-                V[s] = 1/3 * (V[s-8] + V[s+1] + V[s-1])
-            elif s in [8,16,24,32,40,48]:
-                #pl, pd, pr, pu = 0, 1/3, 1/3, 1/3
-                V[s] = 1/3 * (V[s-8] + V[s+1] + V[s+8])
-            elif s in [15,23,31,39,47,55]:
-                #pl, pd, pr, pu = 1/3, 1/3, 0, 1/3
-                V[s] = 1/3 * (V[s-8] + V[s-1] + V[s+8])
-            #other
-            else:
-                #pl, pd, pr, pu = 1/4, 1/4, 1/4, 1/4
-                V[s] = 1/4 * (V[s-8] + V[s+8] + V[s-1] + V[s+1])
+            V[s] = (policy[s][0] * V[left] +
+                    policy[s][1] * V[down] +
+                    policy[s][2] * V[right] +
+                    policy[s][3] * V[up])
 
             #
             diff = max(diff, np.abs(v - V[s]))
@@ -110,7 +109,9 @@ for i in range(10):
     env.render()
     state = env.reset()
 
-    V = policy_evaluation(env.observation_space.n)
+    #policy = equiprobable_policy()
+    policy = certain_policy()
+    V = policy_evaluation(env.observation_space.n, policy)
     print("V: {}".format(V))
     action = getAction(state, V)
 
@@ -125,3 +126,5 @@ for i in range(10):
         env.render() #print last position
         time.sleep(2)
         break
+
+
