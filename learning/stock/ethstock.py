cfaafd04518fc4424a80ab2022e2e8c9d05b8ee7
Samuel Gil <samuxiii@gmx.com>
2018-05-22 19:30:51 +0200

    Editorial and printf

--- learning/stock/ethstock.py
+++ learning/stock/ethstock.py
@@ -5,16 +5,17 @@ import pandas as pd
 import sklearn
 import requests
 
-def getData():
+def gettingData():
     url = "https://www.coingecko.com/price_charts/export/279/eur.csv"
     content = requests.get(url).content
     data = pd.read_csv(io.StringIO(content.decode('utf-8')))
-    print(data.head())
-
-
+    return data
 
 def main():
-    getData()
+    data = gettingData()
+
+    print("Retrieved data:")
+    print(data.tail())
 
 
 if __name__ == "__main__":
