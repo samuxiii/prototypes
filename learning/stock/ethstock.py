11323a28d90ed59f32d1120224c2f63bdbed0564
Samuel Gil <samuxiii@gmx.com>
2018-05-23 20:43:17 +0200

    Index is completed if there is no sample for a date

--- learning/stock/ethstock.py
+++ learning/stock/ethstock.py
@@ -18,11 +18,34 @@ def preprocessing(data):
     data.set_index('snapped_at', inplace=True)
     data.index = pd.to_datetime(data.index)
 
+    '''
+    In some cases there is no sample for a certain date.
+    '''
+    #Generate all the possible days and use them to reindex
+    start = data.index[data.index.argmin()]
+    end = data.index[data.index.argmax()]
+    
+    index_complete = pd.date_range(start, end)
+    data = data.reindex(index_complete)
+
+    #Fill the blanks with the mean between the previous and the day after
+    print("\nLooking if the index is complete...")
+    for idx in data.index:
+        dayloc = data.index.get_loc(idx)
+        day = data.loc[idx]
+        if day.hasnans:
+            #updating
+            rg = slice(dayloc-1, dayloc+2)
+            data.loc[idx] = data.iloc[rg].mean()
+            print("Day <{}> updated with the mean".format(idx))
+
 def main():
     data = gettingData()
 
-    print("Retrieved data:")
+    print("\nRetrieved data:")
     print(data.tail())
+
+    preprocessing(data)
 
 
 if __name__ == "__main__":
