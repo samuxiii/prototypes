1b7d24569f7f118ba41a0bc2f06c267817501b44
Samuel Gil <samuxiii@gmx.com>
2019-01-19 17:49:36 +0100

    Separate function from kaparthy
    Method for preprocessing the image

--- aigym/pong/pong.py
+++ aigym/pong/pong.py
@@ -4,15 +4,8 @@ import random
 import numpy as np
 from time import sleep
 from nn import get_nn
+from kaparthy import prepro
 
-def prepro(I):
-# prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector
-  I = I[35:195] # crop
-  I = I[::2,::2,0] # downsample by factor of 2
-  I[I == 144] = 0 # erase background (background type 1)
-  I[I == 109] = 0 # erase background (background type 2)
-  I[I != 0] = 1 # everything else (paddles, ball) just set to 1
-  return I.astype(np.float).ravel()
 
 # code for the two only actions in Pong
 UP_ACTION = 2
