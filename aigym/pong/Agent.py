5db073fb46b6dcb9a7bb185a38ae5096b6537a35
Samuel Gil <samuxiii@gmx.com>
2019-02-16 01:22:22 +0100

    Cleaning things and Conv2D

--- aigym/pong/Agent.py
+++ aigym/pong/Agent.py
@@ -1,9 +1,10 @@
 import random
 import numpy as np
 from keras import backend as K
-from keras.layers import Dense, Conv2D, Flatten
+from keras.layers import Dense, Conv2D, Flatten, BatchNormalization
 from keras.models import Sequential
 from keras.optimizers import Adam
+
 
 class Agent:
 
@@ -14,22 +15,39 @@ class Agent:
 
     def __model(self):
         model = Sequential()
-        model.add(Conv2D(16, kernel_size=8, strides=4, activation='relu', input_shape=(80, 80, 3)))
+        model.add(Conv2D(16, kernel_size=8, strides=4, activation='relu', input_shape=(80, 80, 1)))
+        model.add(BatchNormalization())
         model.add(Conv2D(32, kernel_size=4, strides=2, activation='relu'))
+        model.add(BatchNormalization())
         model.add(Flatten())
-        model.add(Dense(256, activation='relu'))
-        model.add(Dense(2, activation='softmax'))
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.add(Dense(128, activation='relu'))
+        model.add(BatchNormalization())
+        model.add(Dense(3, activation='softmax'))
+        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
         return model
 
     def preprocess(self, I):
         # prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector
         I = I[35:195]  # crop
-        I = I[::2, ::2, :]  # downsample by factor of 2
+        I = I[::2, ::2, 0]  # downsample by factor of 2
         I[I == 144] = 0  # erase background (background type 1)
         I[I == 109] = 0  # erase background (background type 2)
         I[I != 0] = 1  # everything else (paddles, ball) just set to 1
-        return I  # shape:(80, 80, 3)
+        I = np.reshape(I, (80, 80, 1))
+        return I  # shape:(80, 80, 1)
+
+    def discount_rewards(self, r, gamma=0.99):
+        """ take 1D float array of rewards and compute discounted reward """
+        discounted_r = np.zeros_like(r)
+        running_add = 0
+        for t in reversed(range(0, len(r))):
+            if r[t] != 0: running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
+            running_add = running_add * gamma + r[t]
+            discounted_r[t] = running_add
+        # normalize
+        discounted_r -= np.mean(discounted_r)
+        discounted_r /= np.std(discounted_r)
+        return discounted_r
 
     def remember(self, state, action, reward, next_state, done):
         # states must be preprocessed
@@ -41,64 +59,47 @@ class Agent:
         # store in memory the different states, actions, rewards...
         self.memory.append((state, action, reward, next_state, done))
 
-    def replay(self, win):
+    def replay(self):
         # fit model from memory
         gamma = 0.5  # importance of the next reward
-        max_batch_size = 512
 
-        # take care the memory could be big, so using minibatch
-        # minibatch = random.sample(self.memory, min(max_batch_size, len(self.memory)))
-        minibatch = self.memory
-        num_steps = len(minibatch)
-
+        # initialize
         list_x_batch, list_y_batch = [], []
-        step = 0
-        print("steps:{}".format(num_steps))
-        for state, action, reward, next_state, done in minibatch:
 
+        # get the list of rewards
+        _, _, list_r_batch, _, _ = zip(*self.memory)
+
+        # print("steps:{}".format(len(self.memory)))
+        for state, action, reward, next_state, done in self.memory:
             state = np.expand_dims(state, axis=0)
-            target = self.model.predict(state)[0]
 
-            # if not done: #calculate discounted reward
-            #    action_values = self.model.predict(next_state)[0]
-            # following the formula of action-value expectation
-            #    reward = reward + gamma * np.amax(action_values)
-
-            # customize the obtained reward with the calculated
-            #
-            # reward for win  : 1/5  2/5  3/5  4/5  5/5
-            #
-            step += 1
-            reg_win = (step / num_steps)
-            reg_loose = 0 #-(step / num_steps)
-
-            target[action] = reg_win if win else reg_loose
+            target = np.zeros([3])  # 0's for up and down => [0, 0]
+            target[action] = 1  # performed action is set to 1
 
             # append
             list_x_batch.append(state)
             list_y_batch.append(target)
 
-            # clean
-            self.memory = []
+        # clean
+        self.memory = []
 
         # train the model
         x_batch = np.vstack(list_x_batch)
         y_batch = np.vstack(list_y_batch)
-        self.model.fit(x_batch, y_batch, verbose=0)
+        r_batch = self.discount_rewards(list_r_batch)
 
-        # decrease exploration rate
-        if self.epsilon > 0.01:
-            self.epsilon *= 0.9997
+        self.model.fit(x_batch, y_batch, sample_weight=r_batch, verbose=0)
 
     def act(self, state):
         # preprocess the sample
         state = self.preprocess(state)
 
         if self.epsilon > np.random.rand():
-            return random.randint(0, 1)
+            return random.randint(0, 2)
 
         # predict the action to do
         state = np.expand_dims(state, axis=0)
         action_values = self.model.predict(state)
+        # print("Predictions:{}".format(action_values))
 
         return np.argmax(action_values)
