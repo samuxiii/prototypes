02d805b871d4e862f6c0e383d3a6d98c72cbccb7
Samuel Gil <samuxiii@gmx.com>
2020-06-28 18:45:56 +0200

    Adding the training method and creating the origin image

--- cellauto/CellAutomata.py
+++ cellauto/CellAutomata.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+
 from torchvision import transforms
 from PIL import Image
 
@@ -23,7 +25,7 @@ class Grid:
         im = transforms.ToTensor()(im)
         # Padding will be used to nitialize other channels to zero
         padding = torch.zeros((CHANNELS - im.shape[0], WIDTH, HEIGHT))
-        self.image = torch.cat([im, padding], 0).unsqueeze(0)
+        self.image = torch.cat([im, padding], 0)
         return self
 
     def load(self, img):
@@ -54,12 +56,13 @@ class CAModel(nn.Module):
 
         self.fc1 = nn.Linear(CHANNELS * 3, 128)
         self.fc2 = nn.Linear(128, 16)
+        torch.nn.init.zeros_(self.fc2.weight)  # paper recommendation
 
-    def forward(self, input, showLayers=False):
-        # inputs = torch.tensor(minibatch, CHANNELS, WIDTH, HEIGHT)
-        sx = F.conv2d(input, self.sobelX_kernel, padding=1, groups=CHANNELS).clamp(0., 1.)
-        sy = F.conv2d(input, self.sobelY_kernel, padding=1, groups=CHANNELS).clamp(0., 1.)
-        id = F.conv2d(input, self.identity_kernel, padding=1, groups=CHANNELS).clamp(0., 1.)
+    def forward(self, input, debug=False, showLayers=False):
+        # Filters
+        sx = F.conv2d(input, self.sobelX_kernel, padding=1, groups=CHANNELS).clamp(0., 1.).detach()
+        sy = F.conv2d(input, self.sobelY_kernel, padding=1, groups=CHANNELS).clamp(0., 1.).detach()
+        id = F.conv2d(input, self.identity_kernel, padding=1, groups=CHANNELS).clamp(0., 1.).detach()
 
         # every pixel will have 48 channels now in perception tensor
         # shape: [batch, 48, 64, 64]
@@ -100,15 +103,41 @@ class CAModel(nn.Module):
             Grid().load((sy)[0]).show("Sobel Y")
             Grid().load((id)[0]).show("Identity")
             Grid().load((diff)[0]).show("Differential Image")
+        if debug or showLayers:
             Grid().load(updated[0]).show("Updated Image")
 
-        Grid().load(updated[0]).show("Updated Image")
         return updated
 
-###
-m = CAModel()
-img = Grid().loadIronman().image
-x = m.forward(img, True)
 
-for _ in range(5):
-    x = m.forward(x, False)
+def train(m, origin, target, debug=False):
+    loss_f = nn.MSELoss()
+    optimizer = optim.Adam(m.parameters(), lr=0.01)
+    target.unsqueeze_(0)
+
+    # Chose random steps in between range
+    n_steps = torch.randint(64, 96, (1,))
+
+    for i in range(n_steps):
+        optimizer.zero_grad()
+        origin = m.forward(origin, debug)
+        loss = loss_f(origin, target)
+        print("({}/{}) MSE Loss: {}".format(i+1, n_steps, loss))
+        loss.backward()
+        optimizer.step()
+
+        Grid().load(origin[0]).show()
+
+
+###
+model = CAModel()
+print("model: {}".format(model))
+img_target = Grid().loadIronman().image
+
+# Initialize origin image
+img_orig = torch.zeros_like(img_target)
+img_orig[3:, WIDTH//2, HEIGHT//2] = 1.0
+#Grid().load(img_orig).show("Origin")
+
+# Adding batch dimension to img_orig
+img_orig.unsqueeze_(0)
+train(model, img_orig, img_target)
