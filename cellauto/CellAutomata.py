7da08bb7d4a968a14a4beef670fc85fec319ec7c
Samuel Gil <samuxiii@gmx.com>
2020-07-01 22:25:36 +0200

    Many improvements
    * Fully connected replaced by convolution layer with kernel 1
    * Alive mask is applied over sum of original image and diff
    * Clamp is only used at the end of the step, final result
    * Grid only uses the 4 channels (RGBA)

--- cellauto/CellAutomata.py
+++ cellauto/CellAutomata.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-#%matplotlib notebook
+# %matplotlib notebook
 
 from torchvision import transforms
 from PIL import Image
@@ -15,7 +15,9 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 CHANNELS = 16
 WIDTH = HEIGHT = 64
 plt.rcParams['toolbar'] = 'None'
-#plt.ion()
+
+
+# plt.ion()
 
 ###
 class Grid:
@@ -25,12 +27,13 @@ class Grid:
         self.image = None  # dim: (CHANNELS, WIDTH, HEIGHT)
 
     def loadIronman(self):
-        im = Image.open("owl.png") # it should be RGBA
-        #im = im.convert('RGBA')
+        im = Image.open("owl.png")  # it should be RGBA
+        # im = im.convert('RGBA')
         im = transforms.ToTensor()(im)
-        # Padding will be used to nitialize other channels to zero
-        padding = torch.zeros((CHANNELS - im.shape[0], WIDTH, HEIGHT))
-        self.load(torch.cat([im, padding], 0))
+        # Padding will be used to initialize other channels to zero
+        # padding = torch.zeros((CHANNELS - im.shape[0], WIDTH, HEIGHT))
+        # self.load(torch.cat([im, padding], 0))
+        self.load(im)
         return self
 
     def load(self, img):
@@ -43,7 +46,8 @@ class Grid:
         plt.imshow(img)
         plt.title(title)
         plt.show()
-        #plt.draw()
+        # plt.draw()
+
 
 ###
 # Model is explained here -> https://distill.pub/2020/growing-ca/
@@ -60,49 +64,39 @@ class CAModel(nn.Module):
         self.sobelY_kernel = filterY.expand(CHANNELS, 1, 3, 3)
         self.identity_kernel = filterId.expand(CHANNELS, 1, 3, 3)
 
-        self.fc1 = nn.Linear(CHANNELS * 3, 128)
-        self.fc2 = nn.Linear(128, 16)
+        self.fc1 = nn.Conv2d(48, 128, 1)
+        self.fc2 = nn.Conv2d(128, 16, 1)
         torch.nn.init.zeros_(self.fc2.weight)  # paper recommendation
+        torch.nn.init.zeros_(self.fc2.bias)  # paper recommendation
 
-    def forward(self, input, debug=False, showLayers=False):
+    def step(self, input, debug=False, showLayers=False):
+        BATCH_LEN = input.shape[0]
         # Filters
-        sx = F.conv2d(input, self.sobelX_kernel, padding=1, groups=CHANNELS).clamp(0., 1.).detach()
-        sy = F.conv2d(input, self.sobelY_kernel, padding=1, groups=CHANNELS).clamp(0., 1.).detach()
-        id = F.conv2d(input, self.identity_kernel, padding=1, groups=CHANNELS).clamp(0., 1.).detach()
+        sx = F.conv2d(input, self.sobelX_kernel, padding=1, groups=CHANNELS).detach()
+        sy = F.conv2d(input, self.sobelY_kernel, padding=1, groups=CHANNELS).detach()
+        id = F.conv2d(input, self.identity_kernel, padding=1, groups=CHANNELS).detach()
 
         # every pixel will have 48 channels now in perception tensor
         # shape: [batch, 48, 64, 64]
-        perception = torch.cat([sx, sx, id], 1)
-        #print(perception.shape)
-        #print(perception[0,:,32,32])
-
-        # Now we need to pass every pixel through a dense layer
-        # so reshaping is needed: [1, 48, 64, 64] -> [batch_pixels, 48]
-        perception = perception.permute(0, 2, 3, 1).squeeze(0).view(WIDTH * HEIGHT, CHANNELS * 3)
-        #print(perception.shape)
-        #print(perception[32 + 32*HEIGHT])
+        perception = torch.cat([id, sx, sy], 1)
+        # print(perception.shape)
 
         x = self.fc1(perception)
+        # x = self.norm1(x)
         x = F.relu(x)
-        x = self.fc2(x)  # No relu, we want also negative values, they are the differential image
-
-        # Reshaping again to get the differential image
-        # so [batch_pixels, 16] -> [1, 16, 64, 64]
-        diff = x.view(1, WIDTH, HEIGHT, CHANNELS).permute(0, 3, 1, 2)
-        #print(diff.shape)
+        diff = self.fc2(x)  # No relu, we want also negative values, they are the differential image
 
         # stochastic update for differential image
-        stochastic = torch.rand((1, 1, WIDTH, HEIGHT)) < 0.5
-        stochastic = stochastic.type(torch.float).to(device)
-        #print(stochastic.shape)
-        diff = diff * stochastic  # same tensor will be applied over all the channels
+        stochastic = torch.rand((BATCH_LEN, 1, WIDTH, HEIGHT)) < 0.5
+        stochastic = stochastic.type(torch.float).repeat(1, 16, 1, 1).to(device)
+        # print("stoch:{}".format(stochastic.shape))
+        output = input + diff * stochastic  # same tensor will be applied over all the channels
 
         # alive masking
-        alive = F.max_pool2d(input[:, 3, :, :], 3, stride=1, padding=1) > 0.1
-        alive = alive.type(torch.float)
-        #print(alive.shape)
-
-        updated = ((diff + id) * alive).clamp(0., 1.)
+        alive = F.max_pool2d(output[:, 3, :, :], 3, stride=1, padding=1) > 0.1
+        alive = alive.type(torch.float).view(BATCH_LEN, 1, 64, 64).repeat(1, 16, 1, 1)
+        # print("alive:{}".format(alive.shape))
+        output *= alive
 
         if showLayers:
             Grid().load((sx)[0]).show("Sobel X")
@@ -110,32 +104,38 @@ class CAModel(nn.Module):
             Grid().load((id)[0]).show("Identity")
             Grid().load((diff)[0]).show("Differential Image")
         if debug or showLayers:
-            Grid().load(updated[0]).show("Updated Image")
+            Grid().load(output[0]).show("Updated Image")
 
-        return updated
+        return torch.clamp(output, 0.0, 1.0)
+
+    def forward(self, input, debug=False, showLayers=False):
+        # Chose random steps in between range
+        n_steps = torch.randint(64, 96, (1,))
+        output = input.to(device)
+
+        for _ in range(n_steps):
+            output = self.step(output)
+
+        return output
 
 
 def train(m, origin, target, debug=False):
-    target = target.unsqueeze_(0).to(device)
-    lossT = 0
+    target = target.to(device)
+
     for epoch in range(1000):
         loss_f = nn.MSELoss()
         optimizer = optim.Adam(m.parameters(), lr=0.0001)
+        optimizer.zero_grad()
 
-        # Chose random steps in between range
-        n_steps = torch.randint(64, 96, (1,))
-        img = origin.clone().detach().to(device)
+        output = m.forward(origin)
 
-        for step in range(n_steps):
-            optimizer.zero_grad()
-            img = m.forward(img, debug)
-            lossT = loss_f(img[:,:4,...], target[:,:4,...]) #loss is only calculated with RGBA
-            lossT.backward()
-            optimizer.step()
+        loss = loss_f(output[:, :4, ...], target[:, :4, ...])  # Loss is only calculated with RGBA
+        loss.backward()
+        optimizer.step()
 
         if epoch % 100 == 0:
-            Grid().load(img[0]).show("Epoch {}".format(epoch))
-            print("Epoch:{} Step:{}/{}) MSE Loss: {}".format(epoch, step + 1, n_steps.data[0], lossT))
+            Grid().load(output[0]).show("Epoch {}".format(epoch))
+            print("Epoch:{} MSE_Loss:{}".format(epoch, loss))
 
 
 ###
@@ -144,11 +144,14 @@ print("model: {} over device={}".format(model, device))
 img_target = Grid().loadIronman().image
 
 # Initialize origin image
-img_orig = torch.zeros_like(img_target)
-img_orig[3:, WIDTH//2, HEIGHT//2] = 1.0
-#Grid().load(img_orig).show("Origin")
+img_orig = torch.zeros(CHANNELS, WIDTH, HEIGHT)
+img_orig[3:, WIDTH // 2, HEIGHT // 2] = 1.0
+# Grid().load(img_orig).show("Origin")
 
-# Adding batch dimension to img_orig
+# Adding batch dimension
 img_orig.unsqueeze_(0)
+img_orig = img_orig.repeat(1, 1, 1, 1)
+img_target.unsqueeze_(0)
+img_target = img_target.repeat(1, 1, 1, 1)
 
 train(model, img_orig, img_target)
